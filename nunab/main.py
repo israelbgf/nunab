f9abb171dc4f56ce747652a03321a995fe36b0e7
Israel Fonseca <israel.bgf@gmail.com>
2021-05-01 18:26:20 -0300

    Implemented the actual post to YNAB API, but first we need to enrich the transaction data (categories and stuff)

--- nunab/main.py
+++ nunab/main.py
@@ -1,22 +1,24 @@
 from datetime import date
 
-from core import find_nubank_changes_that_needs_to_be_imported_to_ynab
-from gateways import get_ynab_transactions, get_nubank_transactions, send_changes_to_ynab
+import yaml
+
+from core import find_nubank_changes_that_needs_to_be_imported_to_ynab, convert_nubank_to_yanb
+from gateways import *
+
+NUNAB_CONFIG = yaml.safe_load(open('../config.production.yaml'))
 
 
 def sync_ynab_with_nubank():
     ynab = get_ynab_transactions()
     nubank = get_nubank_transactions()
-    changes = find_nubank_changes_that_needs_to_be_imported_to_ynab(ynab, nubank, valid_range=(date(2021, 4, 22),
-                                                                                               date(2021, 4, 30)))
-    send_changes_to_ynab(changes)
+    transactions = find_nubank_changes_that_needs_to_be_imported_to_ynab(ynab, nubank, valid_range=(date(2021, 5, 1),
+                                                                                                    date(2022, 5, 1)))
+
+    send_changes_to_ynab(list(map(lambda x: convert_nubank_to_yanb(x, NUNAB_CONFIG), transactions)), dryrun=True)
 
 
 if __name__ == '__main__':
-    print(get_nubank_transactions())
-    # sync_ynab_with_nubank()
+    sync_ynab_with_nubank()
+    # list_ynab_categories()
     # download_nubank_transactions()
     # download_ynab_transactions(since='2021-04-01', dest='transactions.ynab.json')
-    # sync_ynab_with_nubank()
-    # pprint(pickle.load(open('transactions.nubank.pickle', 'rb')))
-    # pprint(pickle.load(open('transactions.ynab')))
