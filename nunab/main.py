fbab8a300138bad6dd6db1b334aa5f96d47be7f2
Israel Fonseca <israel.bgf@gmail.com>
2021-05-01 19:00:37 -0300

    Better CLI support.

--- nunab/main.py
+++ nunab/main.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import date
 
 import yaml
@@ -8,17 +9,23 @@ from gateways import *
 NUNAB_CONFIG = yaml.safe_load(open('../config.production.yaml'))
 
 
-def sync_ynab_with_nubank():
+def sync_ynab_with_nubank(dryrun):
     ynab = get_ynab_transactions()
     nubank = get_nubank_transactions()
     transactions = find_nubank_changes_that_needs_to_be_imported_to_ynab(ynab, nubank, valid_range=(date(2021, 5, 1),
                                                                                                     date(2022, 5, 1)))
 
-    send_changes_to_ynab(list(map(lambda x: convert_nubank_to_yanb(x, NUNAB_CONFIG), transactions)), dryrun=True)
+    send_changes_to_ynab(list(map(lambda x: convert_nubank_to_yanb(x, NUNAB_CONFIG), transactions)), dryrun)
 
 
 if __name__ == '__main__':
-    sync_ynab_with_nubank()
-    # list_ynab_categories()
-    # download_nubank_transactions()
-    # download_ynab_transactions(since='2021-04-01', dest='transactions.ynab.json')
+    input = sys.argv[1]
+
+    if input == 'donwload-data':
+        download_nubank_transactions()
+        download_ynab_transactions(since='2021-04-01', dest='transactions.ynab.json')
+        sync_ynab_with_nubank(dryrun=True)
+    if input == 'sync-ynab':
+        sync_ynab_with_nubank(dryrun=False)
+    if input == 'debug-ynab':
+        list_ynab_categories()
